8971c7644a51270ef997bd59d183882496fa0026
devlulcas <lucasalvesrego.contato@gmail.com>
2023-04-20 03:13:44 -0300

    feat: basic frontend feed

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './assets/styles/global.css'
+import { AppRouter } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppRouter/>
   </React.StrictMode>,
 )
